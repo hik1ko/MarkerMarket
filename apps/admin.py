76c5db2b21f669da581864d30ad4c170eb945326
shahzod7176 <qoshqulovshahzod9@gmail.com>
2024-08-13 14:15:48 +0500

    models

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+from apps.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryModelAdmin(ImportExportModelAdmin):
+    actions = ['export_as_csv']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(parent__isnull=True)
+
+
+@admin.register(Product)
+class ProductModelAdmin(ImportExportModelAdmin):
--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import JSONField
+from django.utils.text import slugify
+from django_ckeditor_5.fields import CKEditor5Field
+from mptt.models import MPTTModel
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, editable=False)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            unique = self.slug
+            num = 1
+            while Category.objects.filter(slug=unique).exists():
+                unique = f'{self.slug}-{num}'
+                num += 1
+            self.slug = unique
+        super().save(force_insert, force_update, using, update_fields)
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    title = models.CharField(max_length=355)
+    slug = models.SlugField
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    description = JSONField()
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag = models.ManyToManyField('Tag', related_name='tag')
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def in_stock(self):
+        return self.quantity > 0
+
